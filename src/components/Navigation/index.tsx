bd21480786b07ead1c90aa816e1fc85d1b3ba201
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2022-08-29 22:07:22 -0300

    Prototyping the navigation bar

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Navigation = () => {
+    return (
+        <nav>
+        <ul>
+            <li>Início</li>
+            <li>Lista de Conteúdos</li>
+            <li>Controle de Estudos</li>
+            <li>Cadastro de Conteúdo</li>
+        </ul>
+        </nav>
+    )
+    }
+
+export default Navigation
